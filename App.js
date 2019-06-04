6159f2bc2bba0a3cb7d64aaa037e5dabbf181cbc
Andrey <homin2001@gmail.com>
2019-06-04 22:07:08 +0300

    добавил редакс

--- App.js
+++ App.js
@@ -4,15 +4,24 @@ import {createStore, applyMiddleware} from 'redux';
 import ReduxThunk from 'redux-thunk';
 import Content from './src/test';
 import reducer from './src/redux/reducers/reducer';
+import { createStackNavigator,createAppContainer } from "react-navigation";
 
 const store = createStore(reducer,applyMiddleware(ReduxThunk));
 
+const screens = createStackNavigator({
+  Test: Content
+},{
+  headerMode: 'none',
+  mode: 'modal'
+});
+
+const Navigation =  createAppContainer(screens);
 
 export default class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Content/>
+        <Navigation />
       </Provider>
     );
   }
